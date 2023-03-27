defb4a940fb0cb2d0ce5308827a2d92830bdbf77
Jerome Jaglale <jerome.jaglale@gmail.com>
2023-03-27 22:31:29 +0000

    Add GET endpoint

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected function developers(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => json_decode($value),
+        );
+    }
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,24 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
+// health endpoint
 Route::get('/health', function () {
 	return response()->json([
 	    'status' => 'available',
 	]);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// GET endpoint
+Route::get('/product', function () {
+	return Product::all();
 });
