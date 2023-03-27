ea99843130a8b0031da2b6fd39aec6101465e91d
Jerome Jaglale <jerome.jaglale@gmail.com>
2023-03-27 22:19:09 +0000

    Add /health endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/health', function () {
+	return response()->json([
+	    'status' => 'available',
+	]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
