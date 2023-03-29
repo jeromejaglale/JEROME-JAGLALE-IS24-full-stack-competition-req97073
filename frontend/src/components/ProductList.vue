dfa9edd22f8d3a950c73209f537f1ac594728e2a
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-03-29 14:40:36 -0700

    Add Add/Delete buttons in table

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -2,6 +2,17 @@
 export default {
     data() {
         return {
+	        fields: [
+	          { key: 'product_id', label: 'Id'},
+	          { key: 'product_name', label: 'Name'},
+	          { key: 'product_owner_name', label: 'Owner'},
+	          { key: 'developers', label: 'Developers'},
+	          { key: 'scrum_master_name', label: 'Scrum Master'},
+	          { key: 'start_date', label: 'Start Date'},
+	          { key: 'methodology', label: 'Methodology'},
+	          { key: 'edit', label: '' },
+	          { key: 'delete', label: '' }
+	        ]
       	}
     },
     props: {
@@ -12,6 +23,19 @@ export default {
 
 <template>
   	<h2>Product List</h2>
-    <b-table striped :items="products"></b-table>
+    <b-table striped :items="products" :fields="fields">
+		<template #cell(edit)="row">
+			<div class="text-nowrap">
+			<b-button size="sm">
+			   Edit
+			</b-button>
+			</div>
+		</template>
+		<template #cell(delete)="row">
+			<b-button size="sm">
+			   Delete
+			</b-button>
+		</template>
+	</b-table>
 </template>
 
