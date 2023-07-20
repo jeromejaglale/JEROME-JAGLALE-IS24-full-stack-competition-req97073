00201ca8f1e8e095b7c46e37d316c5451dc3dc5a
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-07-19 18:11:30 -0700

    Refactor product list to separate component

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,51 +1,13 @@
-import React from 'react'
-import './App.css'
-
-const ENDPOINT = 'https://fs.local/api/products';
+import React from 'react';
+import './App.css';
+import ProductList from './components/ProductList';
 
 function App() {
-  
-  // status: idle | loading | success | error
-  const [status, setStatus] = React.useState('idle');
-  const [productList, setProductList] = React.useState([]);
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-   
-    setStatus('loading');
-   
-    const response = await fetch(ENDPOINT);
-    const json = await response.json();
-   
-    if (json) {
-      setStatus('success');
-      console.log(json);
-      setProductList(json);
-    } else {
-      setStatus('error');
-    }
-  }
-   
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="submit"></input>  
-      </form>
-     
-      {status === 'error' && (
-        <p>Something went wrong!</p>
-      )}
-
-      {productList.length} products
-
-      <ul>
-        {productList.map(product => 
-          <li>{product.product_name}</li>
-        )}
-      </ul>
+     <ProductList />
     </>
   ); 
-
 }
 
 export default App
