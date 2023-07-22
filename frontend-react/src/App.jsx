a7691cd96b71909a934dcd4823d62d7ae1310ddd
Jerome Jaglale <jerome_jaglale@sfu.ca>
2023-07-22 16:45:01 -0700

    Bind form fields

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,14 +3,20 @@ import './App.css';
 import ProductList from './components/ProductList';
 
 function App() {
+  const [productName, setProductName] = React.useState('');
+  const [productOwnerName, setProductOwnerName] = React.useState('');
+  const [scrumMasterName, setScrumMasterName] = React.useState('');
+  const [startDate, setStartDate] = React.useState('');
+  const [methodology, setMethodology] = React.useState('');
+
   return (
     <>
       <form>
         <label htmlFor="product_name">Product Name:</label>
-        <input type="text" id="product_name" required />
+        <input type="text" id="product_name" value={productName} onChange={(e) => setProductName(e.target.value)} required />
 
         <label htmlFor="product_owner_name">Product Owner Name:</label>
-        <input type="text" id="product_owner_name" required />
+        <input type="text" id="product_owner_name" value={productOwnerName} onChange={(e) => setProductOwnerName(e.target.value)} required />
 
 {/*
         <label htmlFor="developers">Developers:</label>
@@ -20,13 +26,13 @@ function App() {
 */}
 
         <label htmlFor="scrum_master_name">Scrum Master Name:</label>
-        <input type="text" id="scrum_master_name" required />
+        <input type="text" id="scrum_master_name" value={scrumMasterName} onChange={(e) => setScrumMasterName(e.target.value)} required />
 
         <label htmlFor="start_date">Start Date:</label>
-        <input type="date" id="start_date" required />
+        <input type="date" id="start_date" value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
 
         <label htmlFor="methodology">Methodology:</label>
-        <select id="methodology">
+        <select id="methodology" value={methodology} onChange={(e) => setMethodology(e.target.value)}>
           <option>Agile</option>
           <option>Waterfall</option>
         </select>
